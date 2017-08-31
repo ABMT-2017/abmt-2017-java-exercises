250bda06f4eca3b8b0b8381bd5281dd3c4ec7f16
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2017-08-31 12:28:47 +0200

    add while and do-while

--- src/ch/ethz/ivt/abmt/exercises1/Exercise1_2.java
+++ src/ch/ethz/ivt/abmt/exercises1/Exercise1_2.java
@@ -28,5 +28,19 @@ public class Exercise1_2 {
 		for ( int i = 0; i < myArray.length; i++ ) {
 			System.out.println( "value "+i+" is "+myArray[ i ] );
 		}
+
+		int i = 0;
+		while ( i < myArray.length ) {
+			System.out.println( "value "+i+" is "+myArray[ i ] );
+			i++;
+		}
+
+		// note that this will crash with an array of size 0
+		i = 0;
+		do {
+			System.out.println( "value " + i + " is " + myArray[ i ] );
+			i++;
+		}
+		while ( i < myArray.length );
 	}
 }
